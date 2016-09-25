76f486aeb6eb2fb8679eaf5e051fd64b12cfe1fa
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-25 11:17:10 -0700

    init all display stuff

--- tests/photoresistor/photoresistor.ino
+++ tests/photoresistor/photoresistor.ino
@@ -6,4 +6,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}//libraries
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//init stuff for display
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+void setup() {
+  // put your setup code here, to run once:
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
