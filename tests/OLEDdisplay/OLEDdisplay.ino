fd8bd914f2e61a6f88ed46795eaf3bb7b4d616fc
BobLakensi <montgomery.okdaily@gmail.com>
2016-09-25 11:10:35 -0700

    display prints Hello world

--- tests/OLEDdisplay/OLEDdisplay.ino
+++ tests/OLEDdisplay/OLEDdisplay.ino
@@ -2,9 +2,25 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//init stuff for display
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+const byte DISPLAY_SDA_PIN = 4, DISPLAY_SCK_PIN = 5;
+
 void setup() {
   // put your setup code here, to run once:
-
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("Hello, world!");
+  display.display();
 }
 
 void loop() {
