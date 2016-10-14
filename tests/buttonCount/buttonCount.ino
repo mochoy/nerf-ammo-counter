f2267f173ecd01c2801403b220f688f257c4dbea
BobLakensi <montgomery.okdaily@gmail.com>
2016-10-13 18:17:56 -0700

    init stuff for display

--- tests/buttonCount/buttonCount.ino
+++ tests/buttonCount/buttonCount.ino
@@ -2,6 +2,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//init stuff for display
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
 void setup() {
   // put your setup code here, to run once:
 
