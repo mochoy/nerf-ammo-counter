1826de86502f2116e1621337750bd07c94284560
BobLakensi <montgomery.okdaily@gmail.com>
2016-10-13 18:19:26 -0700

    init button stuff

--- tests/buttonCount/buttonCount.ino
+++ tests/buttonCount/buttonCount.ino
@@ -9,11 +9,18 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
+const byte BTN_PIN = 2;
+int btnState, lastBtnState = LOW; 
+
+unsigned long lastDebounceTime = 0, debounceDelay = 50;
+
 void setup() {
   // put your setup code here, to run once:
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  
+
+  pinMode(BTN_PIN, INPUT);
+
 }
 
 void loop() {
