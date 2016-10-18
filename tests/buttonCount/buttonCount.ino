6a7c37d4a5b883bf6b96bed7c390fc34859171b6
BobLakensi <montgomery.okdaily@gmail.com>
2016-10-17 19:02:43 -0700

    position text based on passed in x and y

--- tests/buttonCount/buttonCount.ino
+++ tests/buttonCount/buttonCount.ino
@@ -43,7 +43,7 @@ void printToScreen (x, y, text) {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 0);
+  display.setCursor(x, y);
   display.print("Hello, world!");
   display.display();
 }
