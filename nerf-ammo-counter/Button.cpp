2099574230fef4adb8fb11e0c362fa48e0abf99f
Monte <montgomery.okdaily@gmail.com>
2016-10-24 23:38:38 +0000

    add basics of c++

--- nerf-ammo-counter/Button.cpp
+++ nerf-ammo-counter/Button.cpp
@@ -1,17 +1,29 @@
 //for Button class
 
+#include <iostream>
+using namespace std;
+
+
+
 class Button {
     public:
         unsigned long lastDebounceTime;
         unsigned long debounceDelay = 50;
         
         int btnState;
-        int lastBtnState = LOW;
+        int lastBtnState = 0;
     
     public:
         void initPin (int pin) {
             const int PIN = pin;
         }
-    
 
+};
+
+
+int main () {
+    
+    cout << "hello" << endl;
+    Button btn1;
 }
+
