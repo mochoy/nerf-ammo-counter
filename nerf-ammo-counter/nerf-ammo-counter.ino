7ca29732480da9fb6bc644c0aa185775c527d326
BobLakensi <montgomery.okdaily@gmail.com>
2016-10-25 18:46:27 -0700

    init stuff for display

--- nerf-ammo-counter/nerf-ammo-counter.ino
+++ nerf-ammo-counter/nerf-ammo-counter.ino
@@ -2,6 +2,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//init stuff for display
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+
 #include "helper.h"
 
 void setup() {
