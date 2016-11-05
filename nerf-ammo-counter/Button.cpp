e9543ca948492cc0c09c7e8b3ad3ec320d0bee21
BobLakensi <montgomery.okdaily@gmail.com>
2016-11-05 13:53:38 -0700

    move Button class to seperate file Button

---
+++ nerf-ammo-counter/Button.cpp
@@ -0,0 +1,52 @@
+class Button {
+    public:
+        int PIN;
+    
+    //constructors
+    public:
+        Button();
+        Button(int);
+    
+    public:
+        unsigned long lastDebounceTime;
+        unsigned long debounceDelay = 50;
+        
+        int btnState;
+        int lastBtnState = LOW;
+    
+    public:
+        bool isBtnPressed() {
+            bool returnVal = false;     //flag so can return at end of method
+            
+            this -> btnState = digitalRead(this -> PIN);
+            
+            //delay
+            if (micros() >= (this -> lastDebounceTime) + (this -> debounceDelay)) {
+                //check if button changes state
+                if (this -> btnState != this -> lastBtnState) {
+                    //check if btn acutally pressed
+                    if (this -> btnState == HIGH) {
+                        returnVal = true;
+                    }
+                }   
+                
+            }
+            
+            this -> lastDebounceTime = micros();
+            this -> lastBtnState = this -> btnState;
+            
+            return returnVal;
+        }   //method
+
+};
+
+//constructors
+Button::Button () {
+    PIN = -1;
+}
+
+Button::Button (int pin) { 
+    PIN = pin;
+
+    pinMode(pin, INPUT);
+}
