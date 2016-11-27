7563020e57a274ab3ba647e742ce10c3c86d8491
BobLakensi <montgomery.okdaily@gmail.com>
2016-11-26 18:17:34 -0800

    add comments so the uninitiated can read

--- nerf-ammo-counter/nerf-ammo-counter.ino
+++ nerf-ammo-counter/nerf-ammo-counter.ino
@@ -1,32 +1,39 @@
-//libraries
+//libraries, so the display can work
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-//init stuff for display
+//initialize stuff for display
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
+//include a seperate file for the other code
 #include "helper.h"
 
+//this code will run when the Arduino turns on
 void setup() {
   // put your setup code here, to run once:
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
+  //set up buttons
   initButtons(3);
+  //show the ammo
   displayAmmo();
 
+  //turn on an LED on the board
   digitalWrite(13, HIGH);
 
 }
 
+//this code loops many times a second
 void loop() {
-  // put your main code here, to run repeatedly:
+  //count ammo, constantly check for the trigger switch to be pressed to count
   countAmmo();
+  //toggle between magazine sizes, constanly check for the magazine toggle switch to be pressed
   toggleMags();
+  //change magazine, constantly check for the magazine switch to be pressed/not pressed
   changeMag();
-//  flashEmptyMag();
   
 }
