56a228363b58f89bcfd72727bb771fa05bea96ed
BobLakensi <montgomery.okdaily@gmail.com>
2017-06-11 14:12:36 -0700

    init all buttons

--- SmartBlaster/SmartBlaster.cpp
+++ SmartBlaster/SmartBlaster.cpp
@@ -7,7 +7,7 @@ SmartBlaster (bool[] modes, byte[] IOPins, byte[] buttons, init[] magSizes) {
 	lastVoltageCheckTime = 0;
 	delayTime = 500;
 
-	initModes().initIOPins();
+	initModes().initIOPins().initButtons();
 }
 
 SmartBlaster& initModes (bool[] modes) {
@@ -31,11 +31,32 @@ SmartBlaster& initIOPins (byte[] pins) {
 	if (_isSelectFire) {
 		_toggleSelectFireInputPin = pins[4];
 		_selectFireOutputPin = pins[5];
+		pinMode(_selectFireOutputPin, OUTPUT);
 	}
 
 	_I2C-SDA-Pin = 4;
     _I2C-SCL-Pin = 5;
 
     return *this;	
+}
+
+SmartBlaster& initButtons () {
+	if (!_isIRGate) {
+		buttonArr[0] = Button(_ammoCountingInputPin);
+	} else {
+		buttonArr[0] = Button(-1);
+	}
+
+	buttonArr[1] = Button(_magInsertionDetectionInputPin);
+	buttonArr[2] = Button(_magSizeToggleInputPin);
+
+	if (_isSelectFire) {
+		buttonArr[3] = Button(_toggleSelectFireInputPin);
+	} else {
+		buttonArr[3] = Button(-1);
+	}
+
+
+	return *this;
 }
 
