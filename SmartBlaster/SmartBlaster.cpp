0fc6e2bbc61f4e8c43ddf85210586b963c3cbee4
BobLakensi <montgomery.okdaily@gmail.com>
2017-06-11 11:45:01 -0700

    add constructor and initModes method

--- SmartBlaster/SmartBlaster.cpp
+++ SmartBlaster/SmartBlaster.cpp
@@ -3,5 +3,17 @@
 
 using namespace SmartBlaster;
 
+SmartBlaster () {
+	lastVoltageCheckTime = 0;
+	delayTime = 500;
+}
 
+initModes (bool isIRGate, bool isChrono, bool isVoltmeter, bool isSelectFire) {
+	_isIRGate = isIRGate;
+	_isChrono = isChrono;
+	_isVoltmeter = isVoltmeter;
+	_isSelectFire = isSelectFire;
+
+	return 
+}
 
